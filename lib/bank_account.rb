f9a66734bd6744fe6f9801faa70558409cca6a4c
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 23:39:04 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
 
-attr_accessor :number, :name, :balance
+attr_accessor :number, :name, :balance, :status
 
 def initialize(name)
   @name = name
   @balance = 1000
+  @status = open
 end
 
 end
