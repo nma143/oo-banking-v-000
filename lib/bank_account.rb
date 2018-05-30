ec9e5fecebd6ad404005625bf661c88a4969dad9
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 23:42:42 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,7 +15,16 @@ end
 
 def display_balance
   "Your balance is $#{@balance}."
+end
 
+def valid?
+
+  if @balance > 0 && @status == "open"
+    return true
+  else  
+    return false
+  end
+  
 end
 
 end
